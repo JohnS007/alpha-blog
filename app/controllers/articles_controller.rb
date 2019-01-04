8079d9b0d233d4d3d19e7b6f22c1ffb68bf19626
Johnty Simon <johntysimon90@gmail.com>
2019-01-04 02:59:37 -0500

    Fix create , add errors and show method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,10 +5,22 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    
     #render plain: params[:article].inspect, // it is used to display the submitted text
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end  
+      
+  end
+  
+  def show
+    
+   @article = Article.find(params[:id]) 
   end
   
   private
