2bc4844977b306cc215c02b52a32e153209e0eb7
Johnty Simon <johntysimon90@gmail.com>
2019-01-05 06:21:01 -0500

    extract the controller file and root path added to app

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_article, only:[:edit, :update, :show, :destroy] 
   def index
     @articles = Article.all
   end
@@ -9,11 +10,9 @@ class ArticlesController < ApplicationController
   end
   
   def edit 
-   @article = Article.find(params[:id]) 
   end
   
   def create
-    
     #render plain: params[:article].inspect, // it is used to display the submitted text
     @article = Article.new(article_params)
     
@@ -27,7 +26,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
        flash[:notice] = "Article was successfully updated"
        redirect_to article_path(@article) 
@@ -38,18 +36,21 @@ class ArticlesController < ApplicationController
   end
   
   def show
-   @article = Article.find(params[:id]) 
   end
   
   def destroy  
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
   end
   
   private
-    def article_params
+  
+  def set_article
+    @article = Article.find(params[:id]) 
+  end
+  
+  def article_params
     params.require(:article).permit(:title, :description)  
-    end
+  end
 end
