90c628f6936b6f092b840011b2fb98a7fba4ce51
Johnty <johntysimon80@gmail.com>
2019-03-20 23:44:06 +0530

    Add edit functionality to users and partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -13,10 +13,23 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.save
+      flash[:success] = "Your account details are updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
 end
