19968959bce5bbc3bea08ee9a35a67acd9ec8865
Johnty <johntysimon80@gmail.com>
2019-03-21 23:00:10 +0530

    Complete authentication system

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
 
   before_action :set_article, only:[:edit, :update, :show, :destroy] # set the @article for the methods specified in only:
+  before_action :require_user, except:[:index,:show]
+  before_action :require_same_user, only:[:edit,:update,:destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -16,7 +18,7 @@ class ArticlesController < ApplicationController
     # debugger // used to set debugger
     # render plain: params[:article].inspect, // it is used to display the submitted text
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -53,5 +55,12 @@ class ArticlesController < ApplicationController
 
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can edit or delete your own articles"
+      redirect_to root_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  
+
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, except: [:new] # to get every other path except the signup path which is created by us.resources
 
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  
 end
