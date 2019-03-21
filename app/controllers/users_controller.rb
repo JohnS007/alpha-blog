bb789710e3820cdf4a3102d722cd6af50c8e775a
Johnty <johntysimon80@gmail.com>
2019-03-21 12:04:50 +0530

    Create show action and add gravatar for Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,15 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.save
-      flash[:success] = "Your account details are updated"
+      flash[:success] = "Your account details are updated!"
       redirect_to articles_path
     else
       render 'edit'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
   private
   def user_params
